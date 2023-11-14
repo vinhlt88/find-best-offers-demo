6ad4d74060be08981b3f3f3972a0cb07c6d86867
Vinh Luu <vinhlt3@gmail.com>
2023-11-14 09:42:34 +0700

    a

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App() {
   const [least_total_amount_enabled, setleast_total_amount_enabled] = useState(true);
   const [least_total_amount_weight, setleast_total_amount_weight] = useState(0.1);
 
-  const [deprioritize_offer_has_cancelled_enabled, setdeprioritize_offer_has_cancelled_enabled] = useState(true);
-  const [deprioritize_offer_has_cancelled_weight, setdeprioritize_offer_has_cancelled_weight] = useState(-100);
-  const [deprioritize_offer_has_cancelled_within_hours, setdeprioritize_offer_has_cancelled_within_hours] = useState(24);
   const [trading_volume_portion_enabled, settrading_volume_portion_enabled] = useState(true);
   const [trading_volume_portion_weight, settrading_volume_portion_weight] = useState(50);
 
@@ -22,14 +19,14 @@ function App() {
   const [completion_time_block_in_seconds_enabled, setcompletion_time_block_in_seconds_enabled] = useState(true);
   const [completion_time_block_in_seconds_weight, setcompletion_time_block_in_seconds_weight] = useState(30);
 
+  const [deprioritize_offer_has_cancelled_enabled, setdeprioritize_offer_has_cancelled_enabled] = useState(true);
+  const [deprioritize_offer_has_cancelled_weight, setdeprioritize_offer_has_cancelled_weight] = useState(-100);
+  const [deprioritize_offer_has_cancelled_within_hours, setdeprioritize_offer_has_cancelled_within_hours] = useState(24);
+
   const renderElementWithWeightOnly = (name, enable, setEnableFunc, weight, setWeightFunc) => {
     return (
-      <div className="input-group">
-        <div className="input-group-prepend">
-          <div className="input-group-text">
-            <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
-          </div>
-        </div>
+      <div className='col'>
+        <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
         <lablel>{name}</lablel>
         <input type="text" className="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
       </div>
@@ -49,6 +46,10 @@ function App() {
     )
   };
 
+  const offerList =[
+
+  ]
+
   return (
     <div className="container">
       <div className="row">
@@ -56,6 +57,10 @@ function App() {
           {renderElementWithWeightOnly("same_bank", same_bank_enabled, setsame_bank_enabled, same_bank_weight, setsame_bank_weight)}
           {renderElementWithWeightOnly("online_or_auto", online_or_auto_enabled, setonline_or_auto_enabled, online_or_auto_weight, setonline_or_auto_weight)}
           {renderElementWithWeightOnly("least_total_amount", least_total_amount_enabled, setleast_total_amount_enabled, least_total_amount_weight, setleast_total_amount_weight)}
+
+          {renderElementWithWeightOnly("trading_volume_portion", trading_volume_portion_enabled, settrading_volume_portion_enabled, trading_volume_portion_weight, settrading_volume_portion_weight)}
+          {renderElementWithWeightOnly("released_rate", released_rate_enabled, setreleased_rate_enabled, released_rate_weight, setreleased_rate_weight)}
+          {renderElementWithWeightOnly("completion_time_block_in_seconds", completion_time_block_in_seconds_enabled, setcompletion_time_block_in_seconds_enabled, completion_time_block_in_seconds_weight, setcompletion_time_block_in_seconds_weight)}
 
           {renderElementWithWeightAndExtra(
             "deprioritize_offer_has_cancelled", 
@@ -67,10 +72,8 @@ function App() {
             deprioritize_offer_has_cancelled_within_hours,
             setdeprioritize_offer_has_cancelled_within_hours,
             )}
-         
-          {renderElementWithWeightOnly("trading_volume_portion", trading_volume_portion_enabled, settrading_volume_portion_enabled, trading_volume_portion_weight, settrading_volume_portion_weight)}
-          {renderElementWithWeightOnly("released_rate", released_rate_enabled, setreleased_rate_enabled, released_rate_weight, setreleased_rate_weight)}
-          {renderElementWithWeightOnly("completion_time_block_in_seconds", completion_time_block_in_seconds_enabled, setcompletion_time_block_in_seconds_enabled, completion_time_block_in_seconds_weight, setcompletion_time_block_in_seconds_weight)}
+
+            <button>Find Best Offers</button>
         </div>
         <div className="col">
           
