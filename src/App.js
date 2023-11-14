36c03bb8e541010c3437327e5fdc3bfa7f6c1811
Vinh Luu <vinhlt3@gmail.com>
2023-11-14 09:24:03 +0700

    a

--- src/App.js
+++ src/App.js
@@ -10,30 +10,58 @@ function App() {
   const [least_total_amount_enabled, setleast_total_amount_enabled] = useState(true);
   const [least_total_amount_weight, setleast_total_amount_weight] = useState(0.1);
 
+  const [deprioritize_offer_has_cancelled_enabled, setdeprioritize_offer_has_cancelled_enabled] = useState(true);
+  const [deprioritize_offer_has_cancelled_weight, setdeprioritize_offer_has_cancelled_weight] = useState(-100);
+  const [deprioritize_offer_has_cancelled_within_hours, setdeprioritize_offer_has_cancelled_within_hours] = useState(24);
+
   const renderElementWithWeightOnly = (name, enable, setEnableFunc, weight, setWeightFunc) => {
     return (
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <div class="input-group-text">
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">
             <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
           </div>
         </div>
         <lablel>{name}</lablel>
-        <input type="text" class="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
+        <input type="text" className="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
+      </div>
+    )
+  };
+
+  const renderElementWithWeightAndExtra = (name, enable, setEnableFunc, weight, setWeightFunc, extra_name, extra_value, extraSetFunc) => {
+    return (
+      <div className='col'>
+        <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
+        <lablel>{name}</lablel>
+        <lablel>weight:</lablel>
+        <input type="text" className="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
+        <lablel>{extra_name}:</lablel>
+        <input type="text" className="form-control" value={extra_value} onChange={e => extraSetFunc(e.target.value)}/>
       </div>
     )
   };
 
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-2">
+    <div className="container">
+      <div className="row">
+        <div className="col-2">
           {renderElementWithWeightOnly("same_bank", same_bank_enabled, setsame_bank_enabled, same_bank_weight, setsame_bank_weight)}
           {renderElementWithWeightOnly("online_or_auto", online_or_auto_enabled, setonline_or_auto_enabled, online_or_auto_weight, setonline_or_auto_weight)}
           {renderElementWithWeightOnly("least_total_amount", least_total_amount_enabled, setleast_total_amount_enabled, least_total_amount_weight, setleast_total_amount_weight)}
+
+          {renderElementWithWeightAndExtra(
+            "deprioritize_offer_has_cancelled", 
+            deprioritize_offer_has_cancelled_enabled,
+            setdeprioritize_offer_has_cancelled_enabled,
+            deprioritize_offer_has_cancelled_weight,
+            setdeprioritize_offer_has_cancelled_weight,
+            "within_hours",
+            deprioritize_offer_has_cancelled_within_hours,
+            setdeprioritize_offer_has_cancelled_within_hours,
+            )}
          
         </div>
-        <div class="col">
+        <div className="col">
           
         </div>
       </div>
