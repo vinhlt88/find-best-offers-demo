b9eacaf687eea4ca51697141ede2cd86133fdd75
Vinh Luu <vinhlt3@gmail.com>
2023-11-14 11:23:27 +0700

    row

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
 
 function App() {
   const [same_bank_enabled, setsame_bank_enabled] = useState(true);
@@ -37,88 +42,146 @@ function App() {
 
   const renderElementWithWeightOnly = (name, enable, setEnableFunc, weight, setWeightFunc) => {
     return (
-      <div className='col'>
-        <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
-        <lablel>{name}</lablel>
-        <input type="text" className="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
-      </div>
+      <Row>
+        <Col >
+          <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
+          <lablel>&emsp;{name}</lablel>
+        </Col>
+        <Col xs={8}>
+          <input type="text" className="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
+        </Col>
+      </Row>
     )
   };
 
   const renderElementWithWeightAndExtra = (name, enable, setEnableFunc, weight, setWeightFunc, extra_name, extra_value, extraSetFunc) => {
     return (
-      <div className='col'>
-        <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
-        <lablel>{name}</lablel>
-        <lablel>weight:</lablel>
-        <input type="text" className="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
-        <lablel>{extra_name}:</lablel>
-        <input type="text" className="form-control" value={extra_value} onChange={e => extraSetFunc(e.target.value)}/>
-      </div>
+      <Form>
+        <Row>
+          <Col >
+            <input type="checkbox" checked={enable} onChange={ e => setEnableFunc(e.target.checked)}/>
+            <lablel>&ensp;{name}</lablel>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <lablel>&nbsp;&emsp;weight:</lablel>
+          </Col>
+          <Col xs={8}>
+            <input type="text" className="form-control" value={weight} onChange={e => setWeightFunc(e.target.value)}/>
+          </Col>
+        </Row>  
+        <br />
+        <Row>
+          <Col>
+            <lablel>&nbsp;&emsp;{extra_name}:</lablel>
+          </Col>
+          <Col xs={8}>
+          <input type="text" className="form-control" value={extra_value} onChange={e => extraSetFunc(e.target.value)}/>
+          </Col>
+        </Row>  
+      </Form>
     )
   };
 
-  const offerList =[
-
-  ]
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-2">
-          {renderElementWithWeightOnly("same_bank", same_bank_enabled, setsame_bank_enabled, same_bank_weight, setsame_bank_weight)}
-          {renderElementWithWeightOnly("online_or_auto", online_or_auto_enabled, setonline_or_auto_enabled, online_or_auto_weight, setonline_or_auto_weight)}
-          {renderElementWithWeightOnly("least_total_amount", least_total_amount_enabled, setleast_total_amount_enabled, least_total_amount_weight, setleast_total_amount_weight)}
-          {renderElementWithWeightOnly("trading_volume_portion", trading_volume_portion_enabled, settrading_volume_portion_enabled, trading_volume_portion_weight, settrading_volume_portion_weight)}
-          {renderElementWithWeightOnly("released_rate", released_rate_enabled, setreleased_rate_enabled, released_rate_weight, setreleased_rate_weight)}
-          {renderElementWithWeightOnly("completion_time_block_in_seconds", completion_time_block_in_seconds_enabled, setcompletion_time_block_in_seconds_enabled, completion_time_block_in_seconds_weight, setcompletion_time_block_in_seconds_weight)}
-          {renderElementWithWeightAndExtra(
-            "deprioritize_offer_has_cancelled",
-            deprioritize_offer_has_cancelled_enabled,
-            setdeprioritize_offer_has_cancelled_enabled,
-            deprioritize_offer_has_cancelled_weight,
-            setdeprioritize_offer_has_cancelled_weight,
-            "within_hours",
-            deprioritize_offer_has_cancelled_within_hours,
-            setdeprioritize_offer_has_cancelled_within_hours,
-          )}
-          {renderElementWithWeightAndExtra(
-            "least_disputed_rate",
-            least_disputed_rate_enabled,
-            setleast_disputed_rate_enabled,
-            least_disputed_rate_weight,
-            setleast_disputed_rate_weight,
-            "within_hours",
-            least_disputed_rate_within_hours,
-            setleast_disputed_rate_within_hours,
-          )}
-          {renderElementWithWeightAndExtra(
-            "taker_fiat_trading_amount",
-            taker_fiat_trading_amount_enabled,
-            settaker_fiat_trading_amount_enabled,
-            taker_fiat_trading_amount_weight,
-            settaker_fiat_trading_amount_weight,
-            "within_hours",
-            taker_fiat_trading_amount_within_hours,
-            settaker_fiat_trading_amount_within_hours,
-          )}
-          {renderElementWithWeightAndExtra(
-            "effective_max_amount",
-            effective_max_amount_enabled,
-            seteffective_max_amount_enabled,
-            effective_max_amount_weight,
-            seteffective_max_amount_weight,
-            "within_hours",
-            effective_max_amount_within_hours,
-            seteffective_max_amount_within_hours,
-          )}
+      <br />
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+        variant="secondary"
+      >
+        Click here
+      </Button>
 
-          <button>Find Best Offers</button>
-        </div>
-        <div className="col">
+      <Collapse in={open}>
+        <Form>
+          <br />
+          <Row>
+            {renderElementWithWeightOnly("same_bank", same_bank_enabled, setsame_bank_enabled, same_bank_weight, setsame_bank_weight)}
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightOnly("online_or_auto", online_or_auto_enabled, setonline_or_auto_enabled, online_or_auto_weight, setonline_or_auto_weight)}
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightOnly("least_total_amount", least_total_amount_enabled, setleast_total_amount_enabled, least_total_amount_weight, setleast_total_amount_weight)}
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightOnly("trading_volume_portion", trading_volume_portion_enabled, settrading_volume_portion_enabled, trading_volume_portion_weight, settrading_volume_portion_weight)}
+          </Row>
+          <br />
           
-        </div>
-      </div>
+          <Row>
+            {renderElementWithWeightOnly("released_rate", released_rate_enabled, setreleased_rate_enabled, released_rate_weight, setreleased_rate_weight)}
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightOnly("completion_time_block_in_seconds", completion_time_block_in_seconds_enabled, setcompletion_time_block_in_seconds_enabled, completion_time_block_in_seconds_weight, setcompletion_time_block_in_seconds_weight)}
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightAndExtra(
+              "deprioritize_offer_has_cancelled",
+              deprioritize_offer_has_cancelled_enabled,
+              setdeprioritize_offer_has_cancelled_enabled,
+              deprioritize_offer_has_cancelled_weight,
+              setdeprioritize_offer_has_cancelled_weight,
+              "within_hours",
+              deprioritize_offer_has_cancelled_within_hours,
+              setdeprioritize_offer_has_cancelled_within_hours,
+            )}        
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightAndExtra(
+              "least_disputed_rate",
+              least_disputed_rate_enabled,
+              setleast_disputed_rate_enabled,
+              least_disputed_rate_weight,
+              setleast_disputed_rate_weight,
+              "within_hours",
+              least_disputed_rate_within_hours,
+              setleast_disputed_rate_within_hours,
+            )}
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightAndExtra(
+              "taker_fiat_trading_amount",
+              taker_fiat_trading_amount_enabled,
+              settaker_fiat_trading_amount_enabled,
+              taker_fiat_trading_amount_weight,
+              settaker_fiat_trading_amount_weight,
+              "within_hours",
+              taker_fiat_trading_amount_within_hours,
+              settaker_fiat_trading_amount_within_hours,
+            )}       
+          </Row>
+          <br />
+          <Row>
+            {renderElementWithWeightAndExtra(
+              "effective_max_amount",
+              effective_max_amount_enabled,
+              seteffective_max_amount_enabled,
+              effective_max_amount_weight,
+              seteffective_max_amount_weight,
+              "within_hours",
+              effective_max_amount_within_hours,
+              seteffective_max_amount_within_hours,
+            )}
+          </Row>
+          <br />
+          <Button variant="secondary">Find Best Offers</Button>
+        </Form>
+      </Collapse>
     </div>
   );
 }
